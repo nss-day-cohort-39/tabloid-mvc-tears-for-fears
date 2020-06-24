e6b5d1373b297dc58fe674ff6da9c05f96e4e31c
unknown <buckleyderekp@gmail.com>
2020-06-24 12:50:33 -0500

    tag list created

---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+namespace TabloidMVC.Repositories
+{
+    public class TagRepository : BaseRepository
+    {
+        public TagRepository(IConfiguration config) : base(config) { }
+
+        
+
+        public List<Tag> GetAllTags()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT id, name FROM Tag ORDER BY Name";
+                    var reader = cmd.ExecuteReader();
+
+                    var tags = new List<Tag>();
+
+                    while (reader.Read())
+                    {
+                        tags.Add(new Tag()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("name")),
+                        });
+                    }
+
+                    reader.Close();
+
+                    return tags;
+                }
+            }
+        }
+    }
+}
