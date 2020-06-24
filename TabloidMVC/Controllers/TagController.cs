9127d3972702ccfdbb9ba64c4db6eb1ce80d4ab7
unknown <buckleyderekp@gmail.com>
2020-06-24 14:55:06 -0500

    tag view edit create delete

--- TabloidMVC/Controllers/TagController.cs
+++ TabloidMVC/Controllers/TagController.cs
@@ -19,32 +19,34 @@ namespace TabloidMVC.Controllers
 
             _tagRepository = new TagRepository(config);
         }
-        // GET: TagController
+        // GET: Tag
         public ActionResult Index()
         {
             List<Tag> tags = _tagRepository.GetAllTags();
             return View(tags);
         }
 
-        // GET: TagController/Details/5
+        // GET: Tag/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var tag = _tagRepository.GetTagById(id);
+            return View(tag);
         }
 
-        // GET: TagController/Create
+        // GET: Tag/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: TagController/Create
+        // POST: Tag/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(Tag tag)
         {
             try
             {
+                _tagRepository.AddTag(tag);
                 return RedirectToAction(nameof(Index));
             }
             catch
@@ -53,45 +55,50 @@ namespace TabloidMVC.Controllers
             }
         }
 
-        // GET: TagController/Edit/5
+        // GET: Tag/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var tag = _tagRepository.GetTagById(id);
+            return View(tag);
         }
 
-        // POST: TagController/Edit/5
+        // POST: Tag/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(Tag tag)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                _tagRepository.UpdateTag(tag);
+
+                return RedirectToAction("Index");
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                return View(tag);
             }
         }
 
-        // GET: TagController/Delete/5
+        // GET: Tag/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            Tag tag = _tagRepository.GetTagById(id);
+            return View(tag);
         }
 
-        // POST: TagController/Delete/5
+        // POST: Tag/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, Tag tag)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                _tagRepository.DeleteTag(id);
+                return RedirectToAction("Index");
             }
             catch
             {
-                return View();
+                return View(tag);
             }
         }
     }
--- TabloidMVC/Repositories/TagRepository.cs
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -41,5 +41,93 @@ namespace TabloidMVC.Repositories
                 }
             }
         }
+
+        public void AddTag(Tag tag)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Tag (Name)
+                        OUTPUT INSERTED.ID
+                        VALUES (@name)";
+                    cmd.Parameters.AddWithValue("@name", tag.Name);
+                    tag.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
+
+        public Tag GetTagById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, [Name] FROM Tag WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+                    var reader = cmd.ExecuteReader();
+
+                    Tag tag = null;
+
+                    if (reader.Read())
+                    {
+                        tag = NewTagFromReader(reader);
+                    }
+
+                    reader.Close();
+
+                    return tag;
+                }
+            }
+        }
+
+        public void UpdateTag(Tag tag)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                                    UPDATE Tag
+                                    SET [Name] = @name
+                                    WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@name", tag.Name);
+                    cmd.Parameters.AddWithValue("@id", tag.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteTag(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        DELETE FROM Tag WHERE id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private Tag NewTagFromReader(SqlDataReader reader)
+        {
+            return new Tag()
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                Name = reader.GetString(reader.GetOrdinal("Name"))
+            };
+        }
     }
 }
