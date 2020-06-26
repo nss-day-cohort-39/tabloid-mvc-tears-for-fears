337d814b8a6bfdb579a21d6ad6af93c69cf85ed8
Keartroth <mecarrolljr@gmail.com>
2020-06-25 23:42:30 -0500

    Added UserProfile views to edit profile type, deactive or reactivate user profiles, display a list of all active users for admins, and display a list of all deactivated users for admins. Refactored UpdateUserStatus method to throw an exception if the current admin attempts to demote him or herself if they are the last active admin profile. Refactored DeactivateUser method to throw an exception if the current admin attempts to deactivate him or herself if they are the last active admin profile.

---
+++ TabloidMVC/Controllers/UserProfileController.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class UserProfileController : Controller
+    {
+
+        private readonly UserProfileRepository _userProfileRepository;
+
+        public UserProfileController(IConfiguration config)
+        {
+            _userProfileRepository = new UserProfileRepository(config);
+        }
+
+        // GET: UserProfile
+        public ActionResult Index()
+        {
+            if(Int32.Parse(User.FindFirstValue(ClaimTypes.Role)) == 1)
+            {
+                var userProfiles = _userProfileRepository.GetAllActiveUserProfiles();
+                userProfiles.Sort((x, y) => string.Compare(x.DisplayName, y.DisplayName));
+
+                return View(userProfiles);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        public ActionResult ViewDeactivated()
+        {
+            if (Int32.Parse(User.FindFirstValue(ClaimTypes.Role)) == 1)
+            {
+                var userProfiles = _userProfileRepository.GetAllDeactivatedUserProfiles();
+                userProfiles.Sort((x, y) => string.Compare(x.DisplayName, y.DisplayName));
+
+                return View(userProfiles);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        // GET: UserProfile/Details/5
+        public ActionResult Details(int id)
+        {
+            if (Int32.Parse(User.FindFirstValue(ClaimTypes.Role)) == 1)
+            {
+                var userProfile = _userProfileRepository.GetUserById(id);
+                return View(userProfile);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        // GET: UserProfile/Create
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: UserProfile/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
+        }
+
+        // GET: UserProfile/Edit/5
+        public ActionResult Edit(int id)
+        {
+            if (Int32.Parse(User.FindFirstValue(ClaimTypes.Role)) == 1)
+            {
+                var userProfile = _userProfileRepository.GetUserById(id);
+                return View(userProfile);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        // POST: UserProfile/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, UserProfile user)
+        {
+            if (Int32.Parse(User.FindFirstValue(ClaimTypes.Role)) == 1)
+            {
+                var userProfile = _userProfileRepository.GetUserById(id);
+
+                try
+                {
+                    _userProfileRepository.UpdateUserType(userProfile);
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    return View(userProfile);
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        // GET: UserProfile/ActiveStatus/5
+        public ActionResult ActiveStatus(int id)
+        {
+            if (Int32.Parse(User.FindFirstValue(ClaimTypes.Role)) == 1)
+            {
+                var userProfile = _userProfileRepository.GetUserById(id);
+                return View(userProfile);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        // POST: UserProfile/ActiveStatus/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ActiveStatus(int id, UserProfile user)
+        {
+            if (Int32.Parse(User.FindFirstValue(ClaimTypes.Role)) == 1)
+            {
+                var userProfile = _userProfileRepository.GetUserById(id);
+
+                try
+                {
+                    _userProfileRepository.UpdateStatus(userProfile);
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    return View(userProfile);
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        private int GetCurrentUserProfileId()
+        {
+            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return int.Parse(id);
+        }
+    }
+}
